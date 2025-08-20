9396b39fdd07a0e665db519b258ea24d4caf1bd4
callangoren <callang1615@gmail.com>
2025-08-20 15:10:41 +0800

    STRING PYTHON

---
+++ string operation.py
@@ -0,0 +1,39 @@
+x = "HEllo"
+print (x.lower())
+x = "HEllo"
+print (x.upper())
+print(len(x))
+sliced_string = x[3:5:]
+print(sliced_string)
+y = "everyone"
+print(x+y)
+f = 6.5
+i = int(f)
+print(i)
+e = 8
+e = float (e)
+print(e)
+print(bool(e))
+e = (str(e))
+print(str(e))
+print(type(e))
+x = "HEllo"
+print (x.lower())
+p = "OKie"
+print (x.upper())
+print(len(x))
+sliced_string = x[0:2:]
+print(sliced_string)
+c = " is cute!"
+print(p+c)
+f = 7.1
+i = int(f)
+print(i)
+e = 9999
+e = float (e)
+print(e)
+print(bool(e))
+e = (str(e))
+print(str(e))
+print(type(e))
+#callan kara
