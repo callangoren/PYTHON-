152875a091083ed5533c70a29ef587a80e46a461
callangoren <callang1615@gmail.com>
2025-09-03 15:41:15 +0800

    operators.py

---
+++ average.py
@@ -0,0 +1,23 @@
+tree1 = 98
+tree2 = 59
+tree3 = 33
+tree4 = 87
+tree5 = 77
+sum = (tree1+tree2+tree3+tree4+tree5)
+print(sum)
+average = sum/5
+print(average)
+money = int(input("how much money do you want to withdraw"))
+notes100 = int(money/100)
+print("you will get",notes100,"100 peso notes")
+tree21 = int(input("tree21"))
+tree22 = int(input("tree22"))
+tree23 = int(input("tree23"))
+sum = (tree21+tree22+tree23)
+print(sum)
+average = sum/3
+print(average)
+print(average>50)
+if average >40 and average >60:
+ print("hello")
+
---
+++ string operaiton. py.py
@@ -0,0 +1,23 @@
+
+a = 20
+b = 30
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a**b)
+print(a//b)
+print(a%b)
+string2 = "callan is learning python right now"
+print(string2.find("callan"))
+print(string2.find("is"))
+print(string2.find("learning"))
+print(string2.find("python"))
+print(string2.find("right now"))
+replaced_string = string2.replace('learning','studying')
+print(replaced_string)
+replaced_string = string2.replace('right now','already')
+print(replaced_string)
+split_string = string2.split()
+print(split_string)
+print(" ".join(split_string))
