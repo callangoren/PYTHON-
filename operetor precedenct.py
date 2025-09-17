77591bcc412108224e3efded7ac290316dc6e68d
callangoren <callang1615@gmail.com>
2025-09-17 15:42:12 +0800

    operetor precedenct.py

---
+++ operetor precedenct.py
@@ -0,0 +1,17 @@
+x = 5
+t = 40
+r = 27
+e = 85
+b = 3865
+p = x + t / e *b
+print(p)
+numn = int(input("enter the numerator"))
+numd = int(input("enter the denominator"))
+if numn % numd ==0:
+    print(numn,"is divisable by",numd)
+else:
+    print(numn,"is not divisable by",numd )
+a = int(input("enter the speed"))
+b = int(input("enter the speed2"))
+c =int(input("enter the speed3"))
+average = (a + b + c)/3
