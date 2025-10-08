04f0ddb82221286f7e3b9cea0cdd17e876ce9612
callangoren <callang1615@gmail.com>
2025-10-08 18:16:03 +0800

    average2

---
+++ average operator.py
@@ -0,0 +1,13 @@
+#a = int(input("enter the speed"))
+#b = int(input("enter the speed2"))
+#c =int(input("enter the speed3"))
+#average = (a + b + c)/3
+#print("average=",average)
+a = 10
+b = 50
+c = 100
+print(a/c*b)
+print(b*c+a)
+print(c-c*a)
+print(a*b+c)
+print(a*c+b)
