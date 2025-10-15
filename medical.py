80c864731ec276fd3908dfe368be2652d96c579e
callangoren <callang1615@gmail.com>
2025-10-15 15:31:35 +0800

    review.py

--- medical.py
+++ medical.py
@@ -8,7 +8,7 @@ else:
     else: 
         print("Not allowed")
   
-  medical_cause2=input("do you have a medical cause y or n")
+medical_cause2=input("do you have a medical cause y or n")
 attendance=int(input("enter the attendance"))
 if medical_cause2=="y":
     print("you are allowed")
--- review2.py
+++ review2.py
@@ -1,4 +1,4 @@
-i=0
+i=int(input("enter the number"))
 if i>=0:
     print("the number 0 or positive")
     if i==0:
