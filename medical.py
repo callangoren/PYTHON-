0a76f4a724747c234d08f18dc47608f1de6394dc
callangoren <callang1615@gmail.com>
2025-10-15 15:09:59 +0800

    medical.py

---
+++ medical.py
@@ -0,0 +1,20 @@
+medical_cause=input("do you have a medical cause y or n")
+attendance=int(input("enter the attendance"))
+if medical_cause=="y":
+    print("you are allowed")
+else:
+    if attendance>75:
+        print("you are allowed")
+    else: 
+        print("Not allowed")
+  
+  medical_cause2=input("do you have a medical cause y or n")
+attendance=int(input("enter the attendance"))
+if medical_cause2=="y":
+    print("you are allowed")
+else:
+    if attendance>75:
+        print("you are allowed")
+    else: 
+        print("Not allowed")
+  
