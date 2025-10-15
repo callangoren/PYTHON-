4768769962458e9ef6961903b45d7e308ebd932f
callangoren <callang1615@gmail.com>
2025-10-15 15:47:11 +0800

    review3.py

---
+++ review3.py
@@ -0,0 +1,9 @@
+i=int(input("enter the number"))
+if i>=0:
+    print("it is zero or positive")
+    if i==0:
+        print("it is zero")
+    else:
+        print("this is positive")
+else:
+    print("this number is negative")
