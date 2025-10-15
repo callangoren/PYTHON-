7f0b49ff1b64928b69bd3954ae3e89d265ae81d9
callangoren <callang1615@gmail.com>
2025-10-15 16:18:18 +0800

    vending.py

---
+++ vending.py
@@ -0,0 +1,13 @@
+choice = input("enter the choice snack or drink?")
+if choice == "drink":
+    choice2 = input("do you want pepsi or cola")
+    if choice2=="cola":
+        print("please pay 2 dollars")
+    else:
+        print("please pay 3 dollars")
+else:
+    choice3 = input("cookies or chips?")
+    if choice3=="chips":
+        print("please pay 5 dollars")
+    else:
+       print("please pay 6 dollars")
