b6184acfdaae501b8c7b1f79c98d061247dd58cd
callangoren <callang1615@gmail.com>
2025-10-22 13:20:17 +0800

    movie.py

---
+++ movie.py
@@ -0,0 +1,15 @@
+print("welcome to callans movie night!")
+choice = input("enter the movie mincraft movie/exit 8")
+if choice == "platform 8":
+    choice2=input("normal room or exclusive room")
+    if choice2=="normal room":
+        print("please pay 10k")
+    else:
+        print("please pay 50k")
+else:
+    choice3=input(" normal scenes or realistic scenes?")
+    if choice3=="realistic":
+       print("please pay 5k")
+    else:
+        print("please pay 1k")
+
