7ba578143b3ec8e726236a98037ef62ea11019bd
callangoren <callang1615@gmail.com>
2025-12-09 12:24:29 +0800

    2 homework.....py

---
+++ 2 homework.....py
@@ -0,0 +1,8 @@
+age = int(input("give me your age."))
+if age < 10:
+    print("you're a coding superstar!!!")
+elif age < 15:
+    print("you are a coding wiz!")
+
+else:
+    print("you're a big kid now!")
--- homwork......py
+++ homwork......py
@@ -2,8 +2,7 @@ i = int(input("enter the number"))
 if i > 0:
     print("it's a postive number")
 elif i == 0:
-    print("0")
+    print("zero")
  
 else:
     print("it's a negative number")
-    
