d518fa07d52b06c69ad270964383c05a90afea75
callangoren <callang1615@gmail.com>
2025-12-14 09:24:43 +0800

    2 homework.....py

--- 2 homework.....py
+++ 2 homework.....py
@@ -1,8 +1,12 @@
-age = int(input("give me your age."))
+age = int(input("give me your age!"))
 if age < 10:
     print("you're a coding superstar!!!")
 elif age < 15:
     print("you are a coding wiz!")
+elif age > 100:
+    print("Tough luck.")
+elif age > 20:
+    print("GET A JOB.")
 
 else:
     print("you're a big kid now!")
