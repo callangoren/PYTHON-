9aab12f0038a783795bd0ae2deaa4f39c1584ea1
callangoren <callang1615@gmail.com>
2025-12-17 12:53:49 +0800

    project apple.py

--- project apple.py
+++ project apple.py
@@ -1,8 +1,9 @@
-level = input("what is your level?")
-coins_collected = input("how many coins do you have?")
-if level >= "5" and coins_collected >= "10":
+level_input = input("what is your level?")
+coins_collected_input = input("how many coins do you have?")
+level = int(level_input)
+coins_collected = int(coins_collected_input)
+
+if level >= 5 and coins_collected >= 10:
     print("Advance to the next level!")
 else:
     print("keep playing!")
-
-
