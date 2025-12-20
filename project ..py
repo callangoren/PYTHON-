258d24e1d56917800b76151e311030fbbb8b0615
callangoren <callang1615@gmail.com>
2025-12-20 13:31:52 +0800

    project_plant.py

---
+++ project ..py
@@ -0,0 +1,7 @@
+flower = input("Does your flower have sun and water?")
+alive_NOW = input("did it rot?.")
+
+if flower == "yes" and alive_NOW == "no":
+    print("good job plant one more!")
+else:
+    print("plant another one and make it stay alive!")
