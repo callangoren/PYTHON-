b0ba3fe3a2dc695e17a37b3b96b382764388afab
callangoren <callang1615@gmail.com>
2025-12-27 10:29:53 +0800

    project ..py

---
+++ project repeat.py
@@ -0,0 +1,7 @@
+mom_level = input("did you defeat the mom?")
+alive_but_scarred = input("are you still alive?")
+
+if mom_level == "yes" and alive_but_scarred == "yes":
+    print("go to the dad level!")
+else:
+    print("restart and stay alive!")
