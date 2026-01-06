ea1490981cb54dfb04a6be094d366931d7cb623b
callangoren <callang1615@gmail.com>
2026-01-06 11:03:53 +0800

    score.py

---
+++ score.py
@@ -0,0 +1,20 @@
+score = int(input("what's your score?"))
+if score >=90:
+    print("Excellent score!")
+elif score >=70:
+    print("Good score.")
+else:
+    print("Could be more.")
+
+
+
+
+
+
+'''
+Practice:
+Write a program that asks the user for a score
+prints "Excellent" if the score is 90 or above
+prints "Good" if the score is 70 to 89
+prints "Needs practice" if the score is below 70
+'''
