534f17732936b7eb880e3fcd7fc75cf58f567202
callangoren <callang1615@gmail.com>
2026-01-07 20:45:20 +0800

    experiment.py

---
+++ experiment.py
@@ -0,0 +1,49 @@
+#print(0 / 0)
+#print(0 / 0)
+#print(0 / 0)
+#print(0 / 0)
+#print(0 / 0)
+#print(0 / 0)
+#print(0 / 0)
+#print(0 / 0)
+#print(0 / 0)
+#print(0 / 0)
+#print("CD")
+#s = 10
+#f = 5
+#d = 0
+#print(s, f, d)
+m = y = e = a = h = 0
+print(m)
+m = y = e = a = h = 0
+print(m)
+m = y = e = a = h = 0
+print(m)
+m = y = e = a = h = 0
+print(m)
+m = y = e = a = h = 0
+print(m)
+
+m = y = e = a = h = 1
+print(m)
+m = y = e = a = h = 1
+print(m)
+m = y = e = a = h = 1
+print(m)
+m = y = e = a = h = 1
+print(m)
+m = y = e = a = h = 1
+
+print("ohayo goizaimazsu!")
+print("konnichiwa!")
+print("konbanwa!")
+print("oyasuminasai!!!")
+print("if you stayed this long congrats but again...... you wasted your time well um")
+print("peace out")
+print("you're still here well adios now for reals this time mayybe go touch grass!")
+
+
+
+
+
+
