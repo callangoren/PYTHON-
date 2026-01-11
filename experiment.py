26a737caa46584089760fc67ff2c1c7430f53ff6
callangoren <callang1615@gmail.com>
2026-01-11 10:13:01 +0800

    experiment.py

--- experiment.py
+++ experiment.py
@@ -14,6 +14,7 @@
 #d = 0
 #print(s, f, d)
 m = y = e = a = h = 0
+...
 print(m)
 m = y = e = a = h = 0
 print(m)
@@ -41,6 +42,35 @@ print("oyasuminasai!!!")
 print("if you stayed this long congrats but again...... you wasted your time well um")
 print("peace out")
 print("you're still here well adios now for reals this time mayybe go touch grass!")
+...
+error = (input("make an error code"))
+if error == "why":
+    print("ERROR CODE 101 PLEASE TRY AGAIN")
+elif error == "Pikachu":
+    print(" ERROR CODE 151 PIKACHU DIDN'T UNDERSTAND. PLEASE TRY AGAN")
+elif error == "im a boy":
+    print("congrats man")
+elif error == "i am your father":
+    print("ONG DARK VADER IS MY FATHER IM BORN EGO NOOOO")
+elif error == "Gwen":
+    print("Get a boyfriend")
+elif error == "im gay":
+    print("and im human and you don't see me bragging about it")
+
+else:
+    print("ERROR CODE 404 I DO NOT UNDERSTAND. PLEASE TRY AGAIN")
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- weather.py
+++ weather.py
@@ -1,10 +1,11 @@
-is_raining = (input("is it raining?"))
-is_cold = (input("is it cold?"))
+is_raining = input("is it raining?")
+is_cold = input("is it cold?")
 if is_raining == "no" and is_cold == "no":
     print("Enjoy the weather!")
 
 elif is_raining == "yes" and is_cold =="no":
     print("Bring an umbrella.")
+    
 
 elif is_cold == "yes" and is_raining == "no":
     print("Bring a jacket.")
