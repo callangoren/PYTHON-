fdb8c2bec367ea31d8e6a311bf73e8a8f8b4360e
callangoren <callang1615@gmail.com>
2026-01-25 10:20:19 +0800

    List_file.py

---
+++ list_file.py
@@ -0,0 +1,9 @@
+#list_of_strings = ["pikachu", "bannete", "chansey", "gyrados", "gengar", "charizard" ]
+#print(list_of_strings[2])
+#print(list_of_strings[3])
+#print(list_of_strings[4])
+#print(list_of_strings[5],"is weak to", list_of_strings[3])
+
+#print(list_of_strings[0])
+List_of_strings3 = ["red", "orange", "blue"]
+print(List_of_strings3)
